a47e336986e4a8e96fe36e51b9e0f09df154bcdc
Hao Liu <liuhao1990@gmail.com>
2016-10-02 17:48:48 +0800

    Sun Oct  2 17:48:48 CST 2016

--- hash.js
+++ hash.js
@@ -1,1 +1,14 @@
-console.log(111);
+var fs = require('fs');
+
+function code() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 10; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+var configFile = fs.readFileSync('_config.yml', 'utf-8');
+console.log(configFile);
