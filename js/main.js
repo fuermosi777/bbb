e2525e0bfd85213592f6c89cdada803463e5ef96
Hao Liu <liuhao1990@gmail.com>
2016-10-02 17:59:18 +0800

    Sun Oct  2 17:59:18 CST 2016

--- js/main.js
+++ js/main.js
@@ -8,8 +8,4 @@
         var hash = date.getFullYear().toString() + date.getMonth().toString() + date.getDate().toString() + date.getHours().toString();
         document.querySelector('.hero img').src = '/images/hero/img' + (parseInt(hash % 36) + 1) + '.jpg';
     }
-
-    function blockIP() {
-        var add = ['73.241.178.10']
-    }
 })();
