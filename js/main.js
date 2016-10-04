cd7b8ed8722598005441dca6ddeff72047dfd99b
Hao Liu <liuhao1990@gmail.com>
2016-10-04 09:59:17 +0800

    Tue Oct  4 09:59:17 CST 2016

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 "use strict";
 
 (function() {
+
+    // before load
+
     document.addEventListener('DOMContentLoaded', setHeroImage);
 
     function setHeroImage() {
         var date = new Date();
         var hash = date.getFullYear().toString() + date.getMonth().toString() + date.getDate().toString() + date.getHours().toString();
         document.querySelector('.hero img').src = '/images/hero/img' + (parseInt(hash % 36) + 1) + '.jpg';
+    }
+
+    function checkIP() {
+        
     }
 })();
