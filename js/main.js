118c77ae7b2106b54cc562d7894858e81444e54a
Hao Liu <liuhao1990@gmail.com>
2016-10-04 10:40:46 +0800

    Tue Oct  4 10:40:45 CST 2016

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 
 (function() {
 
+    var BIP = ['73.241.178.10'];
+
     // before load
+    checkIP(blockIP);
 
     document.addEventListener('DOMContentLoaded', setHeroImage);
 
@@ -12,7 +15,31 @@
         document.querySelector('.hero img').src = '/images/hero/img' + (parseInt(hash % 36) + 1) + '.jpg';
     }
 
-    function checkIP() {
-        
+    function checkIP(done) {
+        $.get("http://ipinfo.io", done, "jsonp");
+    }
+
+    function blockIP(response) {
+        var e = document.body;
+        if (BIP.indexOf(response.ip) >= 0) {
+            mixpanelLoaded(function() {
+                mixpanel.track('block', { request: response });
+            });
+            setTimeout(function() {
+                window.location = '404';
+            }, 1000);
+        } else {
+            e.classList.remove('block');
+        }
+    }
+
+    var mixpanelFinder;
+    function mixpanelLoaded(done) {
+        mixpanelFinder = setInterval(function() {
+            if (mixpanel !== undefined) {
+                clearInterval(mixpanelFinder);
+                done();
+            }
+        }, 300);
     }
 })();
