e322b623986bb1f070e2e867ad2e78c98baae112
Hao Liu <fuermosi777@users.noreply.github.com>
2017-04-24 09:56:27 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 
     // START
 
-    var CORRECT = 'hao';
+    var CORRECT = '098 071 103 120 078 068 085 052 079 084 069 121 077 119 061 061';
 
     var password = document.querySelector('.password');
 
@@ -17,7 +17,7 @@
     input.addEventListener('keyup', function(e) {
         var value = input.value;
 
-        if (value === CORRECT) {
+        if (value === atob(CORRECT.split(' ').map(code => String.fromCharCode(code)).join(''))) {
             password.parentNode.removeChild(password);
         }
 
