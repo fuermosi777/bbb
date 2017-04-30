daf9a2a0710eda22cb07644a34c2146a327f2d40
fuermosi777 <liuhao1990@gmail.com>
2017-04-29 17:06:45 -0700

    Sat Apr 29 17:06:45 PDT 2017

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-"use strict";
+'use strict';
 
 (function() {
+    function loadPage(pwd) {
+        let hash= Sha1.hash(pwd);
+        let url = hash + '/index.html';
+            
+        $.ajax({
+            url : url,
+            dataType : 'html',
+            success : function(data) {
+                mixpanel.track('correct password');
 
+                window.location = url;
+            },
+            error : function(xhr, ajaxOptions, thrownError) {
+                mixpanel.track('false password', {password: pwd});
+
+                $('.password-input').val('');
+            }
+        });
+    }
+
+    $('.password-input').keypress(function(e) {
+        if (e.which == 13) {
+            loadPage($('.password-input').val());
+        }
+    });
+
+    $('.password-input').focus();
 })();
