77018cdbdd633afe6e8cbe9c18ec94f515f6478a
Hao Liu <liuhao1990@gmail.com>
2021-01-23 21:42:03 -0800

    Sat Jan 23 21:42:03 PST 2021

--- js/photo.js
+++ js/photo.js
@@ -1,6 +1,6 @@
 var photos = [
-  "https://user-images.githubusercontent.com/7303373/105619038-5316fd00-5da3-11eb-81ac-57becc189dcd.png",
-  "https://user-images.githubusercontent.com/7303373/105619035-44c8e100-5da3-11eb-9451-25bb1462078c.png",
+  // "https://user-images.githubusercontent.com/7303373/105619038-5316fd00-5da3-11eb-81ac-57becc189dcd.png",
+  // "https://user-images.githubusercontent.com/7303373/105619035-44c8e100-5da3-11eb-9451-25bb1462078c.png",
 ];
 
 function showPhotos() {
