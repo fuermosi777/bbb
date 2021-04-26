e760d87a8a4dd52c8dfc54679f6c5974723f228a
Hao Liu <hao@haos-macbook-m1.lan>
2021-04-25 17:14:37 -0700

    update

--- js/main.js
+++ js/main.js
@@ -23,23 +23,23 @@ function getHashTag() {
 
 function showDynamic() {
   let hashTag = getHashTag();
-  if (hashTag) {
-    let tags = document.querySelectorAll(".hidden");
-    for (let i = 0; i < tags.length; i++) {
-      let tag = tags[i];
-      if (tag.dataset.show === hashTag || tag.dataset.show === "any") {
-        tag.classList.remove("hidden");
-      } else {
-        tag.parentNode.removeChild(tag);
-      }
-    }
-    // If found hashtag in current path. Append it on-the-fly.
-    let links = document.querySelectorAll("a");
-    for (let i = 0; i < links.length; i++) {
-      let link = links[i];
-      link.href += "#" + hashTag;
+
+  let tags = document.querySelectorAll(".hidden");
+  for (let i = 0; i < tags.length; i++) {
+    let tag = tags[i];
+    if (tag.dataset.show === hashTag || tag.dataset.show === "") {
+      tag.classList.remove("hidden");
+    } else {
+      tag.parentNode.removeChild(tag);
     }
   }
+  // If found hashtag in current path. Append it on-the-fly.
+  let links = document.querySelectorAll("a");
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i];
+    link.href += "#" + hashTag;
+  }
+
 }
 
 function showFeather() {
