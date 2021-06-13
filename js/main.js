705cea62d63a8c23f7068099dcd465462c99f601
fuermosi777 <liuhao1990@gmail.com>
2021-06-13 12:39:43 -0700

    Sun Jun 13 12:39:43 PDT 2021

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 "use strict";
 
-// Load Disqus script and remove show-disqus button if exists.
-function showDisqus() {
-  var d = document,
-    s = d.createElement("script");
-  s.src = "https://wan-an-di-qiu.disqus.com/embed.js";
-  s.setAttribute("data-timestamp", +new Date());
-  (d.head || d.body).appendChild(s);
-
-  var button = d.getElementById("show-disqus");
-  if (button) {
-    button.parentElement.removeChild(button);
-  }
-}
-
 function getHashTag() {
   if (window.location.hash) {
     return window.location.hash.substring(1);
